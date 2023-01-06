43cdc1b4e366fbbaf80e539e24e16436af7ce91a
Rohit Rawat <108264456+rohit123-rawat@users.noreply.github.com>
2023-01-06 15:15:51 +0530

    added react in project

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,11 +1,12 @@
-
-// resources/js/App.jsx
 import React from 'react';
 import { createRoot } from 'react-dom/client'
+import Home from './Components/home/home';
 
 export default function App(){
     return(
-        <h1>How To Install React in Laravel 9 with Vite</h1>
+        <>
+        <Home/>
+        </>
     );
 }
 
