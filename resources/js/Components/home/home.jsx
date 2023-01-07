ba3b9f29ef3d31d3c44d9bf1d61975d983e0e306
Rohit Rawat <108264456+rohit123-rawat@users.noreply.github.com>
2023-01-07 12:53:36 +0530

    added some changes

--- resources/js/Components/home/home.jsx
+++ resources/js/Components/home/home.jsx
@@ -1,13 +1,49 @@
-import React from 'react'
+// import React from 'react'
 
-const Home = () => {
+// const Home = () => {
+//   return (
+//     <div>
+//         <>
+        
+//         </>
+//     </div>
+//   )
+// }
+
+// export default Home
+
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
+function Home() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
-    <div>
-        <>
-        <h1>hello there</h1>
-        </>
-    </div>
-  )
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        Launch demo modal
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
 }
 
 export default Home
